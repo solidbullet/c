6eeab257107dfa40daf0405153411cddc10012a4
solidbullet <691622093@qq.com>
2018-11-13 23:17:26 +0800

    update

---
+++ addlib/add.cpp
@@ -0,0 +1,6 @@
+#include "add.h"
+#include <iostream>
+int add(int a,int b)
+{
+	return a+b;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "./addlib/add.h"
+using namespace std;
+
+int main()
+{
+  int number1 = 10;
+  int number2 = 20;
+  cout << "result is: " << add(number1,number2) << endl;
+  return 0;
+}
